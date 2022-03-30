6b3cff8b4130bccb93523416e92bdbf734d5dab2
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2022-03-30 16:18:14 +0200

    Finished

--- PE3/flush.c
+++ PE3/flush.c
@@ -18,14 +18,65 @@
 #define STR_EQUAL 0
 #define MAX_ARGS 16
 
-// We drew the conclusion that since _t means type, it would be appropriate
-// to call this typedef command_t.
 typedef struct {
     size_t arg_count;
     char name[255];
     char* args[MAX_ARGS];
     bool background_task;
 } command_t;
+
+typedef struct linked_process {
+    char name[255];
+    pid_t pid;
+    struct linked_process* next_process;
+} linked_process_t;
+
+void parse_command(command_t *arg_output, char* command);
+pid_t execute_command(command_t *command);
+
+void insert_process(linked_process_t *head, char name[255], pid_t pid) {
+    
+    linked_process_t *tail = head;
+    while (tail->next_process != NULL) {
+        tail = tail->next_process;
+    }
+    
+    tail->next_process = malloc(sizeof(linked_process_t));
+    linked_process_t *process = tail->next_process;
+    strcpy(process->name, name);
+    process->pid = pid;
+    process->next_process = NULL;
+}
+
+bool delete_process(linked_process_t *head, pid_t pid) {
+    
+    linked_process_t *process = head;
+    linked_process_t *process_prev = process;
+    
+    while (process != NULL) {
+        
+        if (process->pid == pid) {
+            process_prev->next_process = process->next_process;
+            free(process);
+            return true;
+        }
+
+        process_prev = process;
+        process = process->next_process;
+    }
+
+    return false;
+}
+
+void print_processes(linked_process_t *head) {
+    linked_process_t *process = head->next_process;
+    printf("PID     Name\n");
+    printf("------------\n");
+    while (process != NULL) {
+        printf("%05i   %s\n", process->pid, process->name);
+        process = process->next_process;
+    }
+}
 
 void parse_command(command_t *arg_output, char* command) {
     int i = 0; // counts argument
@@ -41,42 +92,48 @@ void parse_command(command_t *arg_output, char* command) {
             j = -1;
             arg_output->args[i] = malloc(255);
         } else {
-            
             arg_output->args[i][j] = x;
         }
         j++;
         h++;
     }
-    j++;
+    // decrement to replace \n with \0
+    j--; 
     arg_output->args[i][j] = '\0';
+
+    // increment to set last pointer to NULL
     i++;
     arg_output->args[i] = NULL;
-    arg_output->arg_count = i;
-    strcpy(arg_output->name, arg_output->args[0]);
-    //printf("\n%s, %s\n", arg_output->args[i-1], x);
 
+    // set arg count
+    arg_output->arg_count = i;
+
+    // set background task 
     if (arg_output->args[i-1][j-1] == '&') {
+        arg_output->args[i-1][j-1] = '\0';
         arg_output->background_task = true;
+    } else {
+        arg_output->background_task = false;
     }
+    strcpy(arg_output->name, arg_output->args[0]);
 }
 
 pid_t execute_command(command_t *command) {
     pid_t pid;
 
-    int in = 0, out = 0;
+    bool in = false, out = false;
     char input[64], output[64];
     int i = 0;
     while (command->args[i] != NULL) {
         if (strcmp(command->args[i], "<") == STR_EQUAL) {
             command->args[i] = NULL;
             strcpy(input, command->args[i + 1]);
-            in = 1;
+            in = true;
         }
         else if(strcmp(command->args[i], ">") == STR_EQUAL) {      
             command->args[i] = NULL;
-            printf("\n!!\n");
             strcpy(output, command->args[i + 1]);
-            out = 1;
+            out = true;
         } 
         i++;
     }
@@ -104,7 +161,7 @@ pid_t execute_command(command_t *command) {
             }
             close(fd1);
         }
-        
+    
         int e = execv(command->name, &(command->args[0]));
         if (e == -1) {
             printf("Command failed.\n");
@@ -118,15 +175,32 @@ pid_t execute_command(command_t *command) {
 
 
 int main() {
-    pid_t child;
+    pid_t pid;
     int exit_status = 0;
     int status = 0;
     command_t command;
     char input[256];
+    
+    linked_process_t head;
+    strcpy(head.name, "head");
+    head.next_process = NULL;
+    head.pid = 0;
+
     while (1) {
         exit_status = 0;
         pthread_t thread;
         char path[256];
+
+        // flush should collect all background processes that have terminated (zombies) and print
+        // their exit status like for the foreground processes of the first subtask.
+        pid_t zombie;
+        do {
+            zombie = waitpid(0, &status, WNOHANG);
+            if (zombie > 0) {
+                delete_process(&head, zombie);
+            }
+        } while (zombie > 0);
+
         getcwd(path, sizeof(path));
         printf("\n%s : ", path);
         if (fgets(&input[0], 256, stdin) == NULL) {
@@ -134,6 +208,7 @@ int main() {
         }
         input[strlen(input)-1] = '\0';
         parse_command(&command, input);
+
         if (strcmp("cd", command.name) == STR_EQUAL) {
             int res = chdir((command.args[1]));
             if (res == -1) {
@@ -144,18 +219,24 @@ int main() {
                 }
             }
         } else if (strcmp("jobs", command.name) == STR_EQUAL) {
-
+            print_processes(&head);
         } else {
-            child = execute_command(&command);
-            waitpid(child, &status, 0);
-            exit_status = WEXITSTATUS(status);
+            pid = execute_command(&command);
+            if (command.background_task) {
+                insert_process(&head, command.name, pid);
+                printf("Started command [%s] in the background", input);
+            } else {
+                waitpid(pid, &status, 0);
+                exit_status = WEXITSTATUS(status);
+                printf("Exit status [%s] = %i\n", input, exit_status);
+            }
         }
-        printf("Exit status [%s] = %i\n", input, exit_status);
-        //printf("\n%d\n", command.background_task);
+        
+        // reset command struct
         for (int i = 0; i < command.arg_count; i++) {
             free(command.args[i]);
         }
-        
+        strcpy(command.name, "");
     }
     return 0;
 }
---
+++ PE3/fuck.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    sleep(5);
+    return 0;
+}
